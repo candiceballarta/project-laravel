10e3b2781dd820174899563fa2f7f6229e329e1e
danielallen24 <delrosario.danielallen@gmail.com>
2020-08-21 17:42:31 +0800

    actor softdeletes

--- app/actors.php
+++ app/actors.php
@@ -3,8 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class actors extends Model
 {
-    //
+    protected $fillable = ['fname', 'lname', 'notes'];
+    use SoftDeletes;
 }
