e4a2ef3e51fd12591dab953d72da42d144371f40
candiceballarta <candiceballarta@gmail.com>
2020-08-27 17:34:09 +0800

    model prod rate

--- app/producers.php
+++ app/producers.php
@@ -3,8 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class producers extends Model
 {
-    //
+    protected $fillable = ['fname', 'lname', 'company'];
+    use SoftDeletes;
 }
--- app/ratings.php
+++ app/ratings.php
@@ -3,8 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ratings extends Model
 {
-    //
+    protected $fillable = ['score', 'comment'];
+    use SoftDeletes;
 }
