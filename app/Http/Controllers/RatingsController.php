63e6b810fd335a87e2d89f2241cc0238b3f7fd1e
danielallen24 <delrosario.danielallen@gmail.com>
2020-09-09 22:20:35 +0800

    Profanity Filter

--- app/Http/Controllers/RatingsController.php
+++ app/Http/Controllers/RatingsController.php
@@ -50,7 +50,7 @@ class RatingsController extends Controller
      */
     public function store(Request $request)
     {
-        $rules = ['score'=>'required','comment'=>'required|max:350|alpha_num'];
+        $rules = ['score'=>'required','comment'=>'required|profanity|max:350|alpha_num'];
         $input = $request->all();
         $validator = Validator::make($input, $rules);
         if ($validator->passes()) {
