79ec067bc478badeb4ac1ced177c95e7488f74b9
candiceballarta <candiceballarta@gmail.com>
2020-09-13 06:52:32 +0800

    001

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -16,63 +16,32 @@
         <div class="row">
             @foreach($movies as $movie)
             <div class="col-md-4">
+
                 <div class="card mb-4 box-shadow bg-dark" style="width: 18rem;">
-                    <img class="card-img-top" src="{{ $movie->getFirstMediaUrl('posters') }}" alt="movie-poster" width="70" height="250">
-                    <div class="card-body">
-                        <h5 class="card-title font-weight-bold" style="color: #FF2340">{{$movie->title}}</h5>
-                        <p class="card-text">{{ $movie->producers->fname." ".$movie->producers->lname }}</p>
-                        <small>{{$movie->year}}</small><br>
-                        <div class="btn-group">
-                            {!! Form::open(array('route' => array('movies.edit',$movie->movie_id),'method'=>'GET')) !!}
-                            <button class="btn btn-sm btn-outline-secondary">Edit</button>
-                            {!! Form::close() !!}
-                            {!! Form::open(array('route' => array('movies.destroy',$movie->movie_id),'method'=>'DELETE')) !!}
-                            <button type="button" class="btn btn-sm btn-outline-secondary">Delete</button>
-                            {!! Form::close() !!}
-                            {!! Form::open(array('route' => array('movies.restore', $movie->movie_id), 'method'=>'GET')) !!}
-                            <button type="button" class="btn btn-sm btn-outline-secondary">Restore</button>
-                            {!! Form::close() !!}
+                    <a href="{{route('movies.show',$movie->movie_id)}}">
+                        <img class="card-img-top" src="{{ $movie->getFirstMediaUrl('posters') }}" alt="movie-poster" width="70" height="250"></a>
+                        <div class="card-body">
+                            <h5 class="card-title font-weight-bold" style="color: #FF2340">{{$movie->title}}</h5>
+                            <p class="card-text">{{ $movie->producers->fname." ".$movie->producers->lname }}</p>
+                            <small>{{$movie->year}}</small><br>
+                            <div class="btn-group">
+                                {!! Form::open(array('route' => array('movies.edit',$movie->movie_id),'method'=>'GET')) !!}
+                                <button class="btn btn-sm btn-outline-secondary">Edit</button>
+                                {!! Form::close() !!}
+                                {!! Form::open(array('route' => array('movies.destroy',$movie->movie_id),'method'=>'DELETE')) !!}
+                                <button type="button" class="btn btn-sm btn-outline-secondary">Delete</button>
+                                {!! Form::close() !!}
+                                {!! Form::open(array('route' => array('movies.restore', $movie->movie_id), 'method'=>'GET')) !!}
+                                <button type="button" class="btn btn-sm btn-outline-secondary">Restore</button>
+                                {!! Form::close() !!}
+                            </div>
                         </div>
                     </div>
+
                 </div>
+                @endforeach
             </div>
-            @endforeach
         </div>
+
     </div>
-    {{-- <div class="table-responsive">
-        <table class="table table-striped table-hover">
-            <thead>
-                <tr>
-                    <th>Movie ID</th>
-                    <th>Title</th>
-                    <th>Producer</th>
-                    <th>Edit</th>
-                    <th>Delete</th>
-                    <th>Restore</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($movies as $movie)
-                <tr>
-                    <td>{{$movie->movie_id}}</td>
-                    <td><a href="{{route('movies.show',$movie->movie_id)}}">{{$movie->title}}</a></td>
-                    <td>{{ $movie->producers->fname." ".$movie->producers->lname }}</td>
-
-                    <td>{!! Form::open(array('route' => array('movies.edit',$movie->movie_id),'method'=>'GET')) !!}
-                        <button><i class="fa fa-pencil-square-o" aria-hidden="true" style="font-size:24px; color:yellow" ></i></button></td>
-                        {!! Form::close() !!}
-                        <td>{!! Form::open(array('route' => array('movies.destroy',$movie->movie_id),'method'=>'DELETE')) !!}
-                            <button ><i class="fa fa-trash-o" aria-hidden="true" style="font-size:24px; color:red" ></i></button></td>
-                            {!! Form::close() !!}
-                            <td>{!! Form::open(array('route' => array('movies.restore', $movie->movie_id), 'method'=>'GET')) !!}
-                                <button><i class="fa fa-undo" aria-hidden="true" style="font-size:24px; color:green" ></i></button></td>
-                                {!! Form::close() !!}
-                            </tr>
-
-                            @endforeach
-                        </tbody>
-                    </table> --}}
-                    {{-- </div> --}}
-                    {{-- <div>{{$movies->links()}}</div> --}}
-                </div>
-                @endsection
+    @endsection
